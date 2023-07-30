830a7774a549063165b8862bd05603c1bec551ac
davidlegarre <davidls920@gmail.com>
2023-07-30 17:40:34 +0200

    Player interaction implemented

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from solver.main import solver
 
 if __name__ == "__main__":
-    words = solver()
-    print(words)
+    solver()
--- solver/main.py
+++ solver/main.py
@@ -1,7 +1,39 @@
 from dictionary.wordextractor import extractwords
 
+message = """
+To input your guesses you must type the 5 letters you've tried,
+if the letter is in the correct position it must be follow by a '+' symbol,
+and if the letter is correct but not in the correct position it must be followed
+by a '-' symbol, otherwise it will guess the letters is not in the word.
+When you've inputted all your guesses press enter to start solving.
+"""
+
+words = ["", "", "", "", ""]
+correct_letters = ["", "", "", "", ""]
+correct_positions = []
+incorrect_letters = []
+
+
+def parse_word(word):
+    for i, letter in enumerate(word):
+        try:
+            if word[i + 1] == "+":
+                correct_letters[i] = letter
+            if word[i + 1] == "-":
+                correct_positions.append(letter)
+            else:
+                incorrect_letters.append(letter)
+        except Exception:
+            pass
+
 
 def solver():
-    words = extractwords()
-    print("Finally")
-    return words
+    dictionary = extractwords()
+    print(message)
+    print("Input the guesses:\n")
+    for _ in range(5):
+        word = input().strip()
+        if word == "\n":
+            break
+        print(word)
+        words.append(word)
